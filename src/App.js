1f04edd838ce3e62b02156dec1bda0b9cdbe7ed2
vkotsev <vasil@3web.bg>
2020-02-28 15:28:30 +0200

    add some functionalities

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import Movies from "./components/movies";
 
 function App() {
-
-
-
   return (
-   <main className="container">
-     <Movies/>
-   </main>
+    <main className="container">
+      <Movies />
+    </main>
   );
 }
 export default App;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,16 +1,62 @@
 import React, { Component } from "react";
-import {getMovies} from "../services/fakeMovieService";
-
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
 
-    state = {
-        movies: getMovies()
-    }
+  constructor() {
+    super();
+    this.handleDelete = this.handleDelete.bind(this);
+  }
 
-    render() {
-        return <div>wdjjdjwdhj</div>
-    }
+  handleDelete(movie) {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies: movies });
+  }
+
+  render() {
+    const { length: countMovies } = this.state.movies;
+    if (countMovies === 0) return <p>There is no movies in the database</p>;
+    else
+      return (
+        <React.Fragment>
+          <p>There is {countMovies} in the database</p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.movies.map(movie => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <button
+                      className="btn btn-danger btn-small"
+                      onClick={function() {
+                        return this.handleDelete(movie);
+                      }.bind(this)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </React.Fragment>
+      );
+  }
 }
 
 export default Movies;
