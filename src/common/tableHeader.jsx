1ab54db89ffbd791708a1492186a72e291cc441e
vkotsev <vasil@3web.bg>
2020-04-07 01:12:39 +0300

    delete commentars

--- src/common/tableHeader.jsx
+++ src/common/tableHeader.jsx
@@ -1,48 +1,5 @@
 import React from "react";
 
-// class TableHeader extends Component {
-//   raiseSort(path) {
-//     const sortColumn = { ...this.props.sortColumn };
-//
-//     if (sortColumn.path === path) {
-//       sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
-//     } else {
-//       sortColumn.path = path;
-//       sortColumn.order = "asc";
-//     }
-//
-//     this.props.onSort(sortColumn);
-//   }
-//
-//   renderSort = column => {
-//     const { sortColumn } = this.props;
-//
-//     if (sortColumn.path !== column.path) return null;
-//     if (sortColumn.order === "asc")
-//       return <i className="fa fa-sort-asc pl-1" />;
-//     return <i className="fa fa-sort-desc pl-1" />;
-//   };
-//
-//   render() {
-//     return (
-//       <thead>
-//         <tr>
-//           {this.props.columns.map(column => (
-//             <th
-//               className="clickable"
-//               key={column.path || column.key}
-//               onClick={() => this.raiseSort(column.path)}
-//             >
-//               {column.label}
-//               {this.renderSort(column)}
-//             </th>
-//           ))}
-//         </tr>
-//       </thead>
-//     );
-//   }
-// }
-
 const TableHeader = ({ sortColumn, columns, onSort }) => {
   const raiseSort = path => {
     sortColumn = { ...sortColumn };
