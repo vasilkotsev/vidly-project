48beecb954b90a82996812f2859b9c4d649159ee
vkotsev <vasil@3web.bg>
2020-04-07 00:41:02 +0300

    add Sort icon in tableHeader(functional component type)

--- src/common/tableHeader.jsx
+++ src/common/tableHeader.jsx
@@ -1,8 +1,51 @@
-import React, { Component } from "react";
+import React from "react";
 
-class TableHeader extends Component {
-  raiseSort(path) {
-    const sortColumn = { ...this.props.sortColumn };
+// class TableHeader extends Component {
+//   raiseSort(path) {
+//     const sortColumn = { ...this.props.sortColumn };
+//
+//     if (sortColumn.path === path) {
+//       sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+//     } else {
+//       sortColumn.path = path;
+//       sortColumn.order = "asc";
+//     }
+//
+//     this.props.onSort(sortColumn);
+//   }
+//
+//   renderSort = column => {
+//     const { sortColumn } = this.props;
+//
+//     if (sortColumn.path !== column.path) return null;
+//     if (sortColumn.order === "asc")
+//       return <i className="fa fa-sort-asc pl-1" />;
+//     return <i className="fa fa-sort-desc pl-1" />;
+//   };
+//
+//   render() {
+//     return (
+//       <thead>
+//         <tr>
+//           {this.props.columns.map(column => (
+//             <th
+//               className="clickable"
+//               key={column.path || column.key}
+//               onClick={() => this.raiseSort(column.path)}
+//             >
+//               {column.label}
+//               {this.renderSort(column)}
+//             </th>
+//           ))}
+//         </tr>
+//       </thead>
+//     );
+//   }
+// }
+
+const TableHeader = ({ sortColumn, columns, onSort }) => {
+  const raiseSort = path => {
+    sortColumn = { ...sortColumn };
 
     if (sortColumn.path === path) {
       sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
@@ -11,25 +54,32 @@ class TableHeader extends Component {
       sortColumn.order = "asc";
     }
 
-    this.props.onSort(sortColumn);
-  }
+    onSort(sortColumn);
+  };
 
-  render() {
-    return (
-      <thead>
-        <tr>
-          {this.props.columns.map(column => (
-            <th
-              key={column.path || column.key}
-              onClick={() => this.raiseSort(column.path)}
-            >
-              {column.label}
-            </th>
-          ))}
-        </tr>
-      </thead>
-    );
-  }
-}
+  const renderSortIcon = column => {
+    if (sortColumn.path !== column.path) return null;
+    if (sortColumn.order === "asc")
+      return <i className="fa fa-sort-asc pl-1" />;
+    return <i className="fa fa-sort-desc pl-1" />;
+  };
+
+  return (
+    <thead>
+      <tr>
+        {columns.map(column => (
+          <th
+            className="clickable"
+            key={column.path || column.key}
+            onClick={() => raiseSort(column.path)}
+          >
+            {column.label}
+            {renderSortIcon(column)}
+          </th>
+        ))}
+      </tr>
+    </thead>
+  );
+};
 
 export default TableHeader;
