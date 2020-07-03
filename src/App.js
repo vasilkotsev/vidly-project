b32ecb8e32fb48209c93312b6e37007d9b0d46d7
vkotsev <vasil@3web.bg>
2020-07-03 16:51:30 +0300

    add register form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Customers from "./components/customers";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
 import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <main className="container">
         <Switch>
           <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/rentals" component={Rentals} />
--- src/components/registerForm.js
+++ src/components/registerForm.js
@@ -2,38 +2,46 @@ import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
 
-class LoginForm extends Form {
+class RegisterForm extends Form {
   state = {
-    data: { username: "", password: "" },
+    data: { username: "", password: "", name: "" },
     errors: {}
   };
 
   schema = {
     username: Joi.string()
       .required()
-      .label("Username"),
+      .label("Username")
+      .email({ minDomainAtoms: 2 }),
     password: Joi.string()
       .required()
       .label("Password")
+      .min(5),
+    name: Joi.string()
+      .required()
+      .label("Name")
   };
 
   doSubmit = () => {
     //call the server and redirect to other page
-    console.log("Submit Login");
+    console.log("Submit Register");
   };
 
   render() {
     return (
       <React.Fragment>
-        <h1>Login</h1>
+        <h1>Register</h1>
         <form onSubmit={this.handleSubmit}>
-          {this.renderInput("username", "Username")}
+          {this.renderInput("username", "Username", "email")}
           {this.renderInput("password", "Password", "password")}
-          {this.renderButton("Login")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
         </form>
       </React.Fragment>
     );
   }
 }
 
-export default LoginForm;
+RegisterForm.propTypes = {};
+
+export default RegisterForm;
