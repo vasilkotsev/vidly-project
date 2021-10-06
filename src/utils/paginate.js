112fada58bf4b630017d49dcfa9eeb068d27ed49
Vasil Kotsev <vasil.kotsev@gmail.com>
2021-10-06 17:06:24 +0300

    refactors paginate util function

--- src/utils/paginate.js
+++ src/utils/paginate.js
@@ -3,8 +3,10 @@ import _ from "lodash";
 export default function paginate(items, currentPage, itemsPerPage) {
   const startIndex = (currentPage - 1) * itemsPerPage;
 
-  return _(items)
+  const paginatedItems = _(items)
     .slice(startIndex)
     .take(itemsPerPage)
     .value();
+
+  return paginatedItems;
 }
